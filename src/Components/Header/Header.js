c7c753842643c76f50f504eb960615819e1a33a0
Alaeddine Messadi <alaeddine.messadi@sparwelt.de>
2018-08-08 13:55:44 +0200

    Timer

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+import 'Header.css'
+import Timer from 'Timer'
+
+const Header = (props) => {
+
+    return (
+        <header>
+            <h1>ToDo List</h1>
+                <Timer/>
+                <p>Looks like another Wednesday</p>
+            </div>
+        </header>
+    )
+}
